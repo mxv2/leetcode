f912ca435a298293ccbeca92ef687beafe4aff0b
Maksim Kiryanov <mxvkiryanov@gmail.com>
2024-09-03 23:26:38 +0200

    136 python

---
+++ 136/python/test_solution.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from solution import Solution
+
+
+class TestSolution(TestCase):
+    solution = Solution()
+
+    def test_single_last(self):
+        result = self.solution.singleNumber([2, 2, 1])
+        self.assertEqual(result, 1, "wrong number")
+
+    def test_single_first(self):
+        result = self.solution.singleNumber([4, 1, 2, 1, 2])
+        self.assertEqual(result, 4, "wrong number")
+
+    def test_single(self):
+        result = self.solution.singleNumber([1])
+        self.assertEqual(result, 1, "wrong number")
+
+    def test_single_negative(self):
+        result = self.solution.singleNumber([2, 2, -1])
+        self.assertEqual(result, -1, "wrong number")
+
+    def test_single_several_negatives(self):
+        result = self.solution.singleNumber([-4, 2, 2, -1, -4])
+        self.assertEqual(result, -1, "wrong number")
+
+    def test_single_min(self):
+        result = self.solution.singleNumber([-4, 2, 2, -30000, -4])
+        self.assertEqual(result, -30000, "wrong number")
+
+    def test_single_max(self):
+        result = self.solution.singleNumber([-4, 2, 2, 30000, -4])
+        self.assertEqual(result, 30000, "wrong number")
